a8197dde78074ae30485d0ab5dba93bde6eef54a
Fitra Ilyasa <nde.joker12@gmail.com>
2024-01-06 18:53:43 +0700

    update

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -38,6 +38,28 @@
                 </div>
             @endforeach
         </div>
+        <h3 class="text-white font-weight-bold pt-4">Ultraman</h3>
+        <div class="text-center d-flex flex-wrap justify-content-center border-bottom">
+            @foreach ($datas->where('category_id', '82') as $data)
+                <a href="#" data-toggle="modal" data-target="#myModal{{ $data->id }}">
+                    <img class="img img-fluid p-3" style="border-radius: 30px" width="300px"
+                        src="{{ asset('assets/img/' . $data->img) }}" alt="{{ $data->img }}">
+                </a>
+
+                <!-- Modal -->
+                <div class="modal fade" id="myModal{{ $data->id }}" tabindex="-1" role="dialog"
+                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-body">
+                                <img class="img img-fluid" src="{{ asset('assets/img/' . $data->img) }}"
+                                    alt="{{ $data->img }}">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
         <h3 class="text-white font-weight-bold pt-4">Super Sentai</h3>
         <div class="text-center d-flex flex-wrap justify-content-center border-bottom">
             @foreach ($datas->where('category_id', '39') as $data)
