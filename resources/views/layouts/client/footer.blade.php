b5f82e3f8acfc9a7504ebc156a968e674b351e8a
fitrailyasa <85192292+fitrailyasa@users.noreply.github.com>
2024-05-16 05:06:50 +0700

    update

--- resources/views/layouts/client/footer.blade.php
+++ resources/views/layouts/client/footer.blade.php
@@ -3,19 +3,19 @@
         <div class="d-flex">
             <ul class="nav col-12 align-items-center justify-content-between">
                 <li><a href="{{ route('beranda') }}" class="nav-link p-3 text-white fw-bold @yield('textHome')"><i
-                            class="fa-solid fa-house fs-4"></i></a>
+                            class="fas fa-home fs-4"></i></a>
                 </li>
                 <li>
                     <a href="{{ route('era') }}" class="nav-link p-3 text-white fw-bold @yield('textEra')"><i
-                            class="fa-solid fa-crown fs-4"></i></a>
+                            class="fas fa-crown fs-4"></i></a>
                 </li>
                 <li>
                     <a href="{{ route('franchise') }}" class="nav-link p-3 text-white fw-bold @yield('textFranchise')"><i
-                            class="fa-solid fa-tv fs-4"></i></a>
+                            class="fas fa-tv fs-4"></i></a>
                 </li>
                 <li>
                     <a href="{{ route('category') }}" class="nav-link p-3 text-white fw-bold @yield('textCategory')"><i
-                            class="fa-solid fa-tag fs-4"></i></a>
+                            class="fas fa-tag fs-4"></i></a>
                 </li>
             </ul>
         </div>
