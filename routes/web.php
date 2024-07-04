c694509824f041089457071f1977acb635a3488a
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2024-07-04 23:13:20 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/franchise/category/{id}', [ClientFranchiseController::class, 'categ
 Route::get('/category', [ClientCategoryController::class, 'index'])->name('category');
 Route::get('/category/{id}', [ClientCategoryController::class, 'show'])->name('category.show');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 Route::middleware(['auth'])->group(function () {
 
@@ -43,59 +43,47 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
 
     // CRUD USER
-    Route::get('/user', [AdminUserController::class, 'index'])->name('user.index');
-    Route::post('/user', [AdminUserController::class, 'store'])->name('user.store');
-    Route::put('/user/{id}/update', [AdminUserController::class, 'update'])->name('user.update');
-    Route::delete('/user/{id}/destroy', [AdminUserController::class, 'destroy'])->name('user.destroy');
-    
+    Route::resource('user', AdminUserController::class)->only(['index', 'store', 'update', 'destroy']);
+
     // CRUD ERA
-    Route::get('/era', [AdminEraController::class, 'index'])->name('era.index');
-    Route::post('/era', [AdminEraController::class, 'store'])->name('era.store');
-    Route::put('/era/{id}/update', [AdminEraController::class, 'update'])->name('era.update');
-    Route::delete('/era/{id}/destroy', [AdminEraController::class, 'destroy'])->name('era.destroy');
+    Route::resource('era', AdminEraController::class)->only(['index', 'store', 'update', 'destroy']);
+    // IMPORT, EXPORT, DELETE ALL ERA
     Route::post('/era/import', [AdminEraController::class, 'import'])->name('era.import');
     Route::get('/era/export', [AdminEraController::class, 'export'])->name('era.export');
     Route::delete('/era/deleteAll', [AdminEraController::class, 'destroyAll'])->name('era.destroyAll');
-    
+
     // CRUD FRANCHISE
-    Route::get('/franchise', [AdminFranchiseController::class, 'index'])->name('franchise.index');
-    Route::post('/franchise', [AdminFranchiseController::class, 'store'])->name('franchise.store');
-    Route::put('/franchise/{id}/update', [AdminFranchiseController::class, 'update'])->name('franchise.update');
-    Route::delete('/franchise/{id}/destroy', [AdminFranchiseController::class, 'destroy'])->name('franchise.destroy');
+    Route::resource('franchise', AdminFranchiseController::class)->only(['index', 'store', 'update', 'destroy']);
+    // IMPORT, EXPORT, DELETE ALL FRANCHISE
     Route::post('/franchise/import', [AdminFranchiseController::class, 'import'])->name('franchise.import');
     Route::get('/franchise/export', [AdminFranchiseController::class, 'export'])->name('franchise.export');
     Route::delete('/franchise/deleteAll', [AdminFranchiseController::class, 'destroyAll'])->name('franchise.destroyAll');
-    
+
     // CRUD CATEGORY
-    Route::get('/category', [AdminCategoryController::class, 'index'])->name('category.index');
-    Route::post('/category', [AdminCategoryController::class, 'store'])->name('category.store');
-    Route::put('/category/{id}/update', [AdminCategoryController::class, 'update'])->name('category.update');
-    Route::delete('/category/{id}/destroy', [AdminCategoryController::class, 'destroy'])->name('category.destroy');
+    Route::resource('category', AdminCategoryController::class)->only(['index', 'store', 'update', 'destroy']);
+    // IMPORT, EXPORT, DELETE ALL CATEGORY
     Route::post('/category/import', [AdminCategoryController::class, 'import'])->name('category.import');
     Route::get('/category/export', [AdminCategoryController::class, 'export'])->name('category.export');
     Route::delete('/category/deleteAll', [AdminCategoryController::class, 'destroyAll'])->name('category.destroyAll');
-    
+
     // CRUD TAG
-    Route::get('/tag', [AdminTagController::class, 'index'])->name('tag.index');
-    Route::post('/tag', [AdminTagController::class, 'store'])->name('tag.store');
-    Route::put('/tag/{id}/update', [AdminTagController::class, 'update'])->name('tag.update');
-    Route::delete('/tag/{id}/destroy', [AdminTagController::class, 'destroy'])->name('tag.destroy');
+    Route::resource('tag', AdminTagController::class)->only(['index', 'store', 'update', 'destroy']);
+    // IMPORT, EXPORT, DELETE ALL TAG
     Route::post('/tag/import', [AdminTagController::class, 'import'])->name('tag.import');
     Route::get('/tag/export', [AdminTagController::class, 'export'])->name('tag.export');
     Route::delete('/tag/deleteAll', [AdminTagController::class, 'destroyAll'])->name('tag.destroyAll');
-    
+
     // CRUD DATA
     Route::get('/data', [AdminDataController::class, 'index'])->name('data.index');
     Route::post('/data', [AdminDataController::class, 'store'])->name('data.store');
     Route::put('/data/{id}/update', [AdminDataController::class, 'update'])->name('data.update');
     Route::delete('/data/{id}/destroy', [AdminDataController::class, 'destroy'])->name('data.destroy');
+    // IMPORT, EXPORT, DELETE ALL DATA
     Route::post('/data/import', [AdminDataController::class, 'import'])->name('data.import');
     Route::get('/data/export', [AdminDataController::class, 'export'])->name('data.export');
     Route::delete('/data/deleteAll', [AdminDataController::class, 'destroyAll'])->name('data.destroyAll');
   });
-  
 });
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/{id}', [HomeController::class, 'show'])->name('beranda.show');
-
