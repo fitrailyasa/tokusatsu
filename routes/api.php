49b25ad29b4614bdfe59df83b53a445de485b4b2
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2025-02-18 14:43:45 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Admin\AdminDataApiController;
 
 Route::post('/login', [LoginApiController::class, 'loginApi'])->name('loginApi');
 
+Route::resource('/', AdminDataApiController::class, ['only', ['index']]);
+
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::resource('data', AdminDataApiController::class, ['only', ['index', 'store', 'show', 'edit', 'update', 'destroy']]);
     Route::post('/logout', [LoginApiController::class, 'logout']);
