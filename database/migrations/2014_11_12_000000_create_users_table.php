94e5e3ce6f3ed794187d76686129885b9d9b14c3
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2025-06-29 04:17:59 +0700

    update

--- database/migrations/2014_11_12_000000_create_users_table.php
+++ database/migrations/2014_11_12_000000_create_users_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
             $table->json('provider')->nullable();
             $table->json('provider_id')->nullable();
             $table->json('provider_token')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
