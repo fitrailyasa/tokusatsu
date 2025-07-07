c3bf0e7ed0c28fd885b292d1e997b5a98e7ada04
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2025-07-07 21:28:42 +0700

    update export icon

--- resources/views/admin/data/excel/export.blade.php
+++ resources/views/admin/data/excel/export.blade.php
@@ -1,6 +1,6 @@
 <!-- Tombol untuk export -->
 <button type="submit" role="button" class="btn btn-sm m-1 btn-success text-white"
-    onclick="event.preventDefault(); document.getElementById('export').submit();"><i class="fas fa-download"></i><span
+    onclick="event.preventDefault(); document.getElementById('export').submit();"><i class="fas fa-file-excel"></i><span
         class="d-none d-sm-inline"> {{ __('Excel') }}</span></button>
 
 <!-- Form export -->
