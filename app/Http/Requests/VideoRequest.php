897fd92357bc73070becd25fd19886aa963058a6
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2025-11-21 22:55:37 +0700

    Update VideoRequest.php

--- app/Http/Requests/VideoRequest.php
+++ app/Http/Requests/VideoRequest.php
@@ -3,7 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use App\Models\video;
+use App\Models\Video;
 use Illuminate\Validation\Rule;
 
 class VideoRequest extends FormRequest
