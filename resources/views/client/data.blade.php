8e12425d326c2b4d2ff700f7b64ed94caba6ab1e
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2025-12-06 06:35:35 +0700

    Update data.blade.php

--- resources/views/client/data.blade.php
+++ resources/views/client/data.blade.php
@@ -1,18 +1,21 @@
 <div class="text-center my-5 py-4">
     <div class="container">
-        <div class="row px-3 mb-3">
-            <div class="col-3 text-left d-flex align-items-center">
+        <div class="row px-3 mb-3 align-items-center">
+            <div class="col-3 text-left">
                 <a href="#" onclick="history.back();">
                     <p class="text-dark m-0"><i class="fas fa-arrow-left"></i></p>
                 </a>
             </div>
+
             <div class="col-6">
                 <h5 class="font-weight-bold responsive-title">{{ $category->fullname }}</h5>
             </div>
+
             <div class="col-3 text-right">
                 <a class="btn btn-xs text-dark rounded aktif"
-                    href="{{ route('video') }}/{{ $category->franchise->slug }}/{{ $category->slug }}"><i
-                        data-feather="play-circle"></i></a>
+                    href="{{ route('video') }}/{{ $category->franchise->slug }}/{{ $category->slug }}">
+                    <i data-feather="play-circle"></i>
+                </a>
             </div>
         </div>
     </div>
@@ -34,7 +37,7 @@
                         <img class="img img-fluid" src="{{ asset('storage/' . $item->img) }}" alt="{{ $item->img }}">
                         <!-- Tombol Download -->
                         <a href="{{ asset('storage/' . $item->img) }}" download="{{ $item->img }}"
-                            class="btn aktif text-dark border my-3 col-12">Download</a>
+                            class="btn aktif text-dark border my-3 col-12">Download Gambar</a>
                         <div>
                             <p>{{ $item->category->description ?? '---------------' }}</p>
                         </div>
