8643135142aed6a7f5bf789e51801159313913ee
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2025-12-07 03:19:46 +0700

    Update buttonSearch.blade.php

--- resources/views/client/buttonSearch.blade.php
+++ resources/views/client/buttonSearch.blade.php
@@ -1,6 +1,6 @@
 <form action="{{ route('search') }}" method="GET">
     <div class="d-flex justify-content-center align-items-center px-3">
-        <div class="col-md-6 input-group mb-3">
+        <div class="col-md-6 input-group mb-2">
             <input type="text" class="form-control" name="query" placeholder="Search..." value="{{ request('query') }}">
             <button class="btn w-25 text-dark aktif border" type="submit">Search</button>
         </div>
