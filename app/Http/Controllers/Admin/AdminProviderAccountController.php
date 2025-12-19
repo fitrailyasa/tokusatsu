6eb9196abddc33c7d577856f67972363b8d8be82
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2025-12-19 16:48:16 +0700

    Update AdminProviderAccountController.php

--- app/Http/Controllers/Admin/AdminProviderAccountController.php
+++ app/Http/Controllers/Admin/AdminProviderAccountController.php
@@ -45,22 +45,34 @@ class AdminProviderAccountController extends Controller
 
     public function callback(Request $request)
     {
-        if ($request->has('code')) {
-            $this->client->fetchAccessTokenWithAuthCode($request->code);
-
-            $oauth2 = new Oauth2($this->client);
-            $userinfo = $oauth2->userinfo->get();
-
-            ProviderAccount::updateOrCreate(
-                ['email' => $userinfo->email],
-                ['access_token' => $this->client->getAccessToken()]
-            );
-
-            return redirect()->route('admin.auth')->with('success', "Account {$userinfo->email} successfully logged in!");
+        if (!$request->has('code')) {
+            return redirect()->route('admin.auth')->with('error', 'Login failed.');
         }
 
-        return redirect()->route('admin.auth')->with('error', 'Login failed.');
+        $token = $this->client->fetchAccessTokenWithAuthCode($request->code);
+
+        if (isset($token['error'])) {
+            return redirect()->route('admin.auth')
+                ->with('error', $token['error_description'] ?? 'Google auth failed.');
+        }
+
+        $this->client->setAccessToken($token);
+
+        $oauth2 = new Oauth2($this->client);
+        $userinfo = $oauth2->userinfo->get();
+
+        ProviderAccount::updateOrCreate(
+            ['email' => $userinfo->email],
+            [
+                'access_token' => json_encode($token),
+            ]
+        );
+
+        return redirect()
+            ->route('admin.auth')
+            ->with('success', "Account {$userinfo->email} successfully logged in!");
     }
+
 
     public function files(Request $request, string $email)
     {
