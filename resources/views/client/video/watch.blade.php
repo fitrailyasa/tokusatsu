a84a65683baad63e4af9e057b8f4ab4a6546e6ad
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2025-12-24 17:24:09 +0700

    Update watch.blade.php

--- resources/views/client/video/watch.blade.php
+++ resources/views/client/video/watch.blade.php
@@ -181,7 +181,7 @@
         document.getElementById("shareBtn").addEventListener("click", async function() {
             const shareData = {
                 title: document.title,
-                text: "{{ $category->fullname }}",
+                text: "{{ $category->fullname - $video->type $video->number }}",
                 url: window.location.href
             };
 
