5056770a6d9fa353d26afe9478f9cbe60570fd1e
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2025-12-31 22:33:50 +0700

    Update search.blade.php

--- resources/views/client/search.blade.php
+++ resources/views/client/search.blade.php
@@ -10,14 +10,25 @@
         <h4 class="text-center responsive-title">Search Result</h4>
         <div class="text-center d-flex flex-wrap justify-content-center py-3">
             @foreach ($datas as $item)
-                <a class="bg-light rounded"
-                    href="{{ url('video/' . $item->category->franchise->slug) }}/{{ $item->category->slug }}/{{ $item->type }}/{{ $item->number }}">
-                    <img class="img img-fluid img-gallery" loading="lazy" src="{{ asset('storage/' . $item->category->img) }}"
-                        alt="{{ $item->category->img }}">
-                    <p class="small px-2">{{ ucwords($item->category->fullname) }} {{ ucwords($item->type) }}
-                        {{ $item->number }}
-                    </p>
-                </a>
+                @if ($item->type == 'episode')
+                    <a class="bg-light rounded"
+                        href="{{ url('video/' . $item->category->franchise->slug) }}/{{ $item->category->slug }}/{{ $item->type }}/{{ $item->number }}">
+                        <img class="img img-fluid img-gallery" loading="lazy"
+                            src="{{ asset('storage/' . $item->category->img) }}" alt="{{ $item->category->img }}">
+                        <p class="small px-2">{{ ucwords($item->category->fullname) }} {{ ucwords($item->type) }}
+                            {{ $item->number }}
+                        </p>
+                    </a>
+                @else
+                    <a class="bg-light rounded"
+                        href="{{ url('video/' . $item->category->franchise->slug) }}/{{ $item->category->slug }}/{{ $item->type }}/{{ $item->number }}">
+                        <img class="img img-fluid img-gallery" loading="lazy"
+                            src="{{ asset('storage/' . $item->category->img) }}" alt="{{ $item->category->img }}">
+                        <p class="small px-2">{{ $item->title }} {{ ucwords($item->type) }}
+                            {{ $item->number }}
+                        </p>
+                    </a>
+                @endif
             @endforeach
         </div>
         <div class="d-flex justify-content-center mt-3">
