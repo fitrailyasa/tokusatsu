0521a35044d4f1ce2054a3f05b9535565e1bec9c
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2026-01-13 20:22:44 +0700

    Update index.blade.php

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -6,113 +6,46 @@
     <div class="container text-center" style="padding-top: 80px; padding-bottom: 80px;">
         @include('client.buttonSearch')
         <br>
-        {{-- ===================== KAMEN RIDER ===================== --}}
-        <div class="section-title">
-            <h5 class="responsive-title">Kamen Rider</h5>
-            <a href="{{ route('video.category', 'kamen-rider') }}">View All <i
-                    class="fa-solid fa-arrow-up-right-from-square"></i></a>
-        </div>
-        <div class="swiper pt-0 pb-3 px-2 mySwiper">
-            <div class="swiper-wrapper">
-                @foreach ($categories as $category)
-                    @if ($category->franchise && $category->franchise->name === 'Kamen Rider')
-                        <div class="swiper-slide">
-                            <a href="{{ route('video.show', [$category->franchise->slug, $category->slug]) }}">
-                                <img class=""
-                                    src="{{ $category->img ? asset('storage/' . $category->img) : asset('storage/comingsoon.jpg') }}"
-                                    loading="lazy" alt="Logo {{ $category->name }}">
-                                <p class="pt-2 normal-text">{{ $category->fullname }}
-                                    @if ($category->first_aired)
-                                        ({{ \Carbon\Carbon::parse($category->first_aired)->year }})
-                                    @endif
-                                </p>
-                            </a>
-                        </div>
-                    @endif
-                @endforeach
-            </div>
-        </div>
 
-        {{-- ===================== ULTRAMAN ===================== --}}
-        <div class="section-title">
-            <h5 class="responsive-title">Ultraman</h5>
-            <a href="{{ route('video.category', 'ultraman') }}">View All <i
-                    class="fa-solid fa-arrow-up-right-from-square"></i></a>
-        </div>
-        <div class="swiper pt-0 pb-3 px-2 mySwiper">
-            <div class="swiper-wrapper">
-                @foreach ($categories as $category)
-                    @if ($category->franchise && $category->franchise->name === 'Ultraman')
-                        <div class="swiper-slide">
-                            <a href="{{ route('video.show', [$category->franchise->slug, $category->slug]) }}">
-                                <img class=""
-                                    src="{{ $category->img ? asset('storage/' . $category->img) : asset('storage/comingsoon.jpg') }}"
-                                    loading="lazy" alt="Logo {{ $category->name }}">
-                                <p class="pt-2 normal-text">{{ $category->fullname }}
-                                    @if ($category->first_aired)
-                                        ({{ \Carbon\Carbon::parse($category->first_aired)->year }})
-                                    @endif
-                                </p>
-                            </a>
-                        </div>
-                    @endif
-                @endforeach
-            </div>
-        </div>
+        @php
+            $franchises = [
+                'Kamen Rider' => 'kamen-rider',
+                'Ultraman' => 'ultraman',
+                'Super Sentai' => 'super-sentai',
+                'Power Rangers' => 'power-rangers',
+            ];
 
-        {{-- ===================== SUPER SENTAI ===================== --}}
-        <div class="section-title">
-            <h5 class="responsive-title">Super Sentai</h5>
-            <a href="{{ route('video.category', 'super-sentai') }}">View All <i
-                    class="fa-solid fa-arrow-up-right-from-square"></i></a>
-        </div>
-        <div class="swiper pt-0 pb-3 px-2 mySwiper">
-            <div class="swiper-wrapper">
-                @foreach ($categories as $category)
-                    @if ($category->franchise && $category->franchise->name === 'Super Sentai')
-                        <div class="swiper-slide">
-                            <a href="{{ route('video.show', [$category->franchise->slug, $category->slug]) }}">
-                                <img class=""
-                                    src="{{ $category->img ? asset('storage/' . $category->img) : asset('storage/comingsoon.jpg') }}"
-                                    loading="lazy" alt="Logo {{ $category->name }}">
-                                <p class="pt-2 normal-text">{{ $category->fullname }}
-                                    @if ($category->first_aired)
-                                        ({{ \Carbon\Carbon::parse($category->first_aired)->year }})
-                                    @endif
-                                </p>
-                            </a>
-                        </div>
-                    @endif
-                @endforeach
-            </div>
-        </div>
+            $groupedCategories = $categories->groupBy(fn($c) => optional($c->franchise)->name);
+        @endphp
 
-        {{-- ===================== POWER RANGERS ===================== --}}
-        <div class="section-title">
-            <h5 class="responsive-title">Power Rangers</h5>
-            <a href="{{ route('video.category', 'power-rangers') }}">View All <i
-                    class="fa-solid fa-arrow-up-right-from-square"></i></a>
-        </div>
-        <div class="swiper pt-0 pb-3 px-2 mySwiper">
-            <div class="swiper-wrapper">
-                @foreach ($categories as $category)
-                    @if ($category->franchise && $category->franchise->name === 'Power Rangers')
+        @foreach ($franchises as $franchiseName => $franchiseSlug)
+            <div class="section-title">
+                <h5 class="responsive-title">{{ $franchiseName }}</h5>
+                <a href="{{ route('video.category', $franchiseSlug) }}">
+                    View All <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                </a>
+            </div>
+
+            <div class="swiper pt-0 pb-3 px-2 mySwiper">
+                <div class="swiper-wrapper">
+                    @foreach ($groupedCategories[$franchiseName] ?? [] as $category)
                         <div class="swiper-slide">
                             <a href="{{ route('video.show', [$category->franchise->slug, $category->slug]) }}">
-                                <img class=""
-                                    src="{{ $category->img ? asset('storage/' . $category->img) : asset('storage/comingsoon.jpg') }}"
+                                <img src="{{ $category->img ? asset('storage/' . $category->img) : asset('storage/comingsoon.jpg') }}"
                                     loading="lazy" alt="Logo {{ $category->name }}">
-                                <p class="pt-2 normal-text">{{ $category->fullname }}
+                                <p class="pt-2 normal-text">
+                                    {{ $category->fullname }}
                                     @if ($category->first_aired)
                                         ({{ \Carbon\Carbon::parse($category->first_aired)->year }})
                                     @endif
                                 </p>
                             </a>
                         </div>
-                    @endif
-                @endforeach
+                    @endforeach
+                </div>
             </div>
-        </div>
+        @endforeach
+
     </div>
 
     <footer class="d-none d-lg-block py-3">
@@ -157,26 +90,23 @@
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
     <script>
-        // Kamen Rider
-        new Swiper(".mySwiper", {
-            slidesPerView: 3,
-            spaceBetween: 10,
-            loop: true,
-            navigation: {
-                nextEl: ".mySwiper .swiper-button-next",
-                prevEl: ".mySwiper .swiper-button-prev",
-            },
-            breakpoints: {
-                768: {
-                    slidesPerView: 3
-                },
-                992: {
-                    slidesPerView: 4
-                },
-                1200: {
-                    slidesPerView: 5
-                },
-            }
+        document.querySelectorAll('.mySwiper').forEach(swiper => {
+            new Swiper(swiper, {
+                slidesPerView: 3,
+                spaceBetween: 10,
+                loop: true,
+                breakpoints: {
+                    768: {
+                        slidesPerView: 3
+                    },
+                    992: {
+                        slidesPerView: 4
+                    },
+                    1200: {
+                        slidesPerView: 5
+                    },
+                }
+            });
         });
     </script>
 
