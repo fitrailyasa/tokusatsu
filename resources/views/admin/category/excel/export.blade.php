b04cfc1c496c84d6a52093b18621ce68c3099ad1
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2026-01-17 05:30:05 +0700

    update

--- resources/views/admin/category/excel/export.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- Tombol untuk export -->
-<button type="submit" role="button" class="btn btn-sm m-1 btn-success text-white"
-    onclick="event.preventDefault(); document.getElementById('export').submit();"><i class="fas fa-file-excel"></i><span
-        class="d-none d-sm-inline"> {{ __('Excel') }}</span></button>
-
-<!-- Form export -->
-<form id="export" action="{{ route('admin.category.exportExcel') }}" hidden>
-    @csrf
-</form>
--- resources/views/components/table-header.blade.php
+++ resources/views/components/table-header.blade.php
@@ -13,21 +13,83 @@
 <!-- Button Import -->
 <x-slot name="import">
     @can('import:' . $permission)
-        @include('admin.' . $permission . '.excel.import')
+        <!-- Button to open modal -->
+        <button role="button" class="btn btn-sm m-1 btn-info" data-bs-toggle="modal" data-bs-target=".formImport"><i
+                class="fas fa-upload"></i> <span class="d-none d-sm-inline">{{ __('Upload') }}</span></button>
+
+        <!-- Modal -->
+        <div class="modal fade formImport" tabindex="-1" role="dialog" aria-labelledby="modalFormLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <form method="POST" action="{{ route('admin.' . $permission . '.import') }}"
+                        enctype="multipart/form-data">
+                        @csrf
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="modalFormLabel">{{ __('Upload Data') }}</h5>
+                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body text-left">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="mb-1">
+                                        <label class="form-label">{{ __('Upload File') }}<span
+                                                class="text-danger">*</span></label>
+                                        <input type="file" accept=".xlsx"
+                                            class="form-control @error('file') is-invalid @enderror" placeholder="file"
+                                            name="file" id="file" required>
+                                        <small class="text-muted">{{ __('Max: 10MB') }}</small>
+                                        @error('file')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <a class="btn btn-success"
+                                href="{{ asset('assets/template/' . $permission . '-template.xlsx') }}"
+                                download="{{ $permission . '-template.xlsx' }}">{{ __('Download Format') }}</a>
+                            <button type="submit" class="btn btn-primary btn-submit">
+                                <span class="spinner-border spinner-border-sm d-none" role="status"
+                                    aria-hidden="true"></span>
+                                <span class="btn-text">{{ __('Save') }}</span>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     @endcan
 </x-slot>
 
 <!-- Button Export Excel -->
 <x-slot name="exportExcel">
     @can('export:' . $permission)
-        @include('admin.' . $permission . '.excel.export')
+        <!-- Tombol untuk export -->
+        <button type="submit" role="button" class="btn btn-sm m-1 btn-success text-white"
+            onclick="event.preventDefault(); document.getElementById('export').submit();"><i
+                class="fas fa-file-excel"></i><span class="d-none d-sm-inline"> {{ __('Excel') }}</span></button>
+
+        <!-- Form export -->
+        <form id="export" action="{{ route('admin.' . $permission . '.exportExcel') }}" hidden>
+            @csrf
+        </form>
     @endcan
 </x-slot>
 
 <!-- Button Export PDF -->
 <x-slot name="exportPDF">
     @can('export:' . $permission)
-        {{-- @include('admin.' . $permission . '.pdf.export') --}}
+        {{-- <!-- Tombol untuk exportPDF -->
+        <button type="submit" role="button" class="btn btn-sm m-1 btn-secondary text-white"
+            onclick="event.preventDefault(); document.getElementById('exportPDF').submit();"><i
+                class="fas fa-file-pdf"></i><span class="d-none d-sm-inline"> {{ __('PDF') }}</span></button>
+
+        <!-- Form exportPDF -->
+        <form id="exportPDF" action="{{ route('admin.' . $permission . '.exportPDF') }}" method="GET" hidden>
+        </form> --}}
     @endcan
 </x-slot>
 
