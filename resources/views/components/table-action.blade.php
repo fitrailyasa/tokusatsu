9808022289b8bf405d6c2ace6490bf13c957e3d6
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2026-01-17 05:47:47 +0700

    update

--- resources/views/components/table-action.blade.php
+++ resources/views/components/table-action.blade.php
@@ -3,25 +3,33 @@
         @if ($item->trashed())
             <!-- Restore and Delete Button -->
             @can('restore:' . $permission)
-                <x-table-modal modalMethod="PUT" route="{{ route('admin.' . $permission . '.restore', $item->id) }}"
-                    id="{{ $item->id }}" modalTitle="Restore Data" modalText="Are you sure you want to restore this data?"
-                    buttonText="Restore" buttonIcon="fa-undo" buttonColor="btn-info" modalType="restore" />
+                @if (Route::has('admin.' . $permission . '.restore'))
+                    <x-table-modal modalMethod="PUT" route="{{ route('admin.' . $permission . '.restore', $item->id) }}"
+                        id="{{ $item->id }}" modalTitle="Restore Data" modalText="Are you sure you want to restore this data?"
+                        buttonText="Restore" buttonIcon="fa-undo" buttonColor="btn-info" modalType="restore" />
+                @endif
             @endcan
             @can('delete:' . $permission)
-                <x-table-modal modalMethod="DELETE" route="{{ route('admin.' . $permission . '.destroy', $item->id) }}"
-                    id="{{ $item->id }}" modalTitle="Permanently Delete Data"
-                    modalText="Are you sure you want to delete data permanently?" buttonText="Delete"
-                    buttonIcon="fa-skull-crossbones" buttonColor="btn-danger" modalType="hardDelete" />
+                @if (Route::has('admin.' . $permission . '.destroy'))
+                    <x-table-modal modalMethod="DELETE" route="{{ route('admin.' . $permission . '.destroy', $item->id) }}"
+                        id="{{ $item->id }}" modalTitle="Permanently Delete Data"
+                        modalText="Are you sure you want to delete data permanently?" buttonText="Delete"
+                        buttonIcon="fa-skull-crossbones" buttonColor="btn-danger" modalType="hardDelete" />
+                @endif
             @endcan
         @else
             <!-- Edit and Soft Delete Buttons -->
             @can('edit:' . $permission)
-                @include('admin.' . $permission . '.edit')
+                @if (Route::has('admin.' . $permission . '.update'))
+                    @include('admin.' . $permission . '.edit')
+                @endif
             @endcan
             @can('soft-delete:' . $permission)
-                <x-table-modal modalMethod="DELETE" route="{{ route('admin.' . $permission . '.softDelete', $item->id) }}"
-                    id="{{ $item->id }}" modalTitle="Delete Data" modalText="Are you sure you want to delete data?"
-                    buttonText="Delete" buttonIcon="fa-trash" buttonColor="btn-danger" modalType="softDelete" />
+                @if (Route::has('admin.' . $permission . '.softDelete'))
+                    <x-table-modal modalMethod="DELETE" route="{{ route('admin.' . $permission . '.softDelete', $item->id) }}"
+                        id="{{ $item->id }}" modalTitle="Delete Data" modalText="Are you sure you want to delete data?"
+                        buttonText="Delete" buttonIcon="fa-trash" buttonColor="btn-danger" modalType="softDelete" />
+                @endif
             @endcan
         @endif
     </td>
--- resources/views/components/table-header.blade.php
+++ resources/views/components/table-header.blade.php
@@ -6,117 +6,129 @@
 <!-- Button Form Create -->
 <x-slot name="formCreate">
     @can('create:' . $permission)
-        @include('admin.' . $permission . '.create')
+        @if (Route::has('admin.' . $permission . '.store'))
+            @include('admin.' . $permission . '.create')
+        @endif
     @endcan
 </x-slot>
 
 <!-- Button Import -->
 <x-slot name="import">
     @can('import:' . $permission)
-        <!-- Button to open modal -->
-        <button role="button" class="btn btn-sm m-1 btn-info" data-bs-toggle="modal" data-bs-target=".formImport"><i
-                class="fas fa-upload"></i> <span class="d-none d-sm-inline">{{ __('Upload') }}</span></button>
+        @if (Route::has('admin.' . $permission . '.import'))
+            <!-- Button to open modal -->
+            <button role="button" class="btn btn-sm m-1 btn-info" data-bs-toggle="modal" data-bs-target=".formImport"><i
+                    class="fas fa-upload"></i> <span class="d-none d-sm-inline">{{ __('Upload') }}</span></button>
 
-        <!-- Modal -->
-        <div class="modal fade formImport" tabindex="-1" role="dialog" aria-labelledby="modalFormLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <form method="POST" action="{{ route('admin.' . $permission . '.import') }}"
-                        enctype="multipart/form-data">
-                        @csrf
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="modalFormLabel">{{ __('Upload Data') }}</h5>
-                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body text-left">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="mb-1">
-                                        <label class="form-label">{{ __('Upload File') }}<span
-                                                class="text-danger">*</span></label>
-                                        <input type="file" accept=".xlsx"
-                                            class="form-control @error('file') is-invalid @enderror" placeholder="file"
-                                            name="file" id="file" required>
-                                        <small class="text-muted">{{ __('Max: 10MB') }}</small>
-                                        @error('file')
-                                            <div class="invalid-feedback">{{ $message }}</div>
-                                        @enderror
+            <!-- Modal -->
+            <div class="modal fade formImport" tabindex="-1" role="dialog" aria-labelledby="modalFormLabel"
+                aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <form method="POST" action="{{ route('admin.' . $permission . '.import') }}"
+                            enctype="multipart/form-data">
+                            @csrf
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="modalFormLabel">{{ __('Upload Data') }}</h5>
+                                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body text-left">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="mb-1">
+                                            <label class="form-label">{{ __('Upload File') }}<span
+                                                    class="text-danger">*</span></label>
+                                            <input type="file" accept=".xlsx"
+                                                class="form-control @error('file') is-invalid @enderror" placeholder="file"
+                                                name="file" id="file" required>
+                                            <small class="text-muted">{{ __('Max: 10MB') }}</small>
+                                            @error('file')
+                                                <div class="invalid-feedback">{{ $message }}</div>
+                                            @enderror
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="modal-footer">
-                            <a class="btn btn-success"
-                                href="{{ asset('assets/template/' . $permission . '-template.xlsx') }}"
-                                download="{{ $permission . '-template.xlsx' }}">{{ __('Download Format') }}</a>
-                            <button type="submit" class="btn btn-primary btn-submit">
-                                <span class="spinner-border spinner-border-sm d-none" role="status"
-                                    aria-hidden="true"></span>
-                                <span class="btn-text">{{ __('Save') }}</span>
-                            </button>
-                        </div>
-                    </form>
+                            <div class="modal-footer">
+                                <a class="btn btn-success"
+                                    href="{{ asset('assets/template/' . $permission . '-template.xlsx') }}"
+                                    download="{{ $permission . '-template.xlsx' }}">{{ __('Download Format') }}</a>
+                                <button type="submit" class="btn btn-primary btn-submit">
+                                    <span class="spinner-border spinner-border-sm d-none" role="status"
+                                        aria-hidden="true"></span>
+                                    <span class="btn-text">{{ __('Save') }}</span>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        @endif
     @endcan
 </x-slot>
 
 <!-- Button Export Excel -->
 <x-slot name="exportExcel">
     @can('export:' . $permission)
-        <!-- Tombol untuk export -->
-        <button type="submit" role="button" class="btn btn-sm m-1 btn-success text-white"
-            onclick="event.preventDefault(); document.getElementById('export').submit();"><i
-                class="fas fa-file-excel"></i><span class="d-none d-sm-inline"> {{ __('Excel') }}</span></button>
+        @if (Route::has('admin.' . $permission . '.exportExcel'))
+            <button type="submit" role="button" class="btn btn-sm m-1 btn-success text-white"
+                onclick="event.preventDefault(); document.getElementById('export').submit();"><i
+                    class="fas fa-file-excel"></i><span class="d-none d-sm-inline"> {{ __('Excel') }}</span></button>
 
-        <!-- Form export -->
-        <form id="export" action="{{ route('admin.' . $permission . '.exportExcel') }}" hidden>
-            @csrf
-        </form>
+            <form id="export" action="{{ route('admin.' . $permission . '.exportExcel') }}" hidden>
+                @csrf
+            </form>
+        @endif
     @endcan
 </x-slot>
 
 <!-- Button Export PDF -->
 <x-slot name="exportPDF">
     @can('export:' . $permission)
-        {{-- <!-- Tombol untuk exportPDF -->
-        <button type="submit" role="button" class="btn btn-sm m-1 btn-secondary text-white"
-            onclick="event.preventDefault(); document.getElementById('exportPDF').submit();"><i
-                class="fas fa-file-pdf"></i><span class="d-none d-sm-inline"> {{ __('PDF') }}</span></button>
+        @if (Route::has('admin.' . $permission . '.exportPDF'))
+            <button type="submit" role="button" class="btn btn-sm m-1 btn-secondary text-white"
+                onclick="event.preventDefault(); document.getElementById('exportPDF').submit();"><i
+                    class="fas fa-file-pdf"></i><span class="d-none d-sm-inline"> {{ __('PDF') }}</span></button>
 
-        <!-- Form exportPDF -->
-        <form id="exportPDF" action="{{ route('admin.' . $permission . '.exportPDF') }}" method="GET" hidden>
-        </form> --}}
+            <form id="exportPDF" action="{{ route('admin.' . $permission . '.exportPDF') }}" method="GET" hidden>
+            </form>
+        @endif
     @endcan
 </x-slot>
 
 <!-- Button Soft Delete All -->
 <x-slot name="softDeleteAll">
     @can('soft-delete-all:' . $permission)
-        <x-table-modal modalMethod="DELETE" route="{{ route('admin.' . $permission . '.softDeleteAll') }}"
-            modalTitle="Delete All Data" modalText="Are you sure you want to delete all data?" buttonText="Delete All"
-            buttonIcon="fa-trash" buttonColor="btn-danger" modalType="softDeleteAll" />
+        @if (Route::has('admin.' . $permission . '.softDeleteAll'))
+            <x-table-modal modalMethod="DELETE" route="{{ route('admin.' . $permission . '.softDeleteAll') }}"
+                modalTitle="Delete All Data" modalText="Are you sure you want to delete all data?" buttonText="Delete All"
+                buttonIcon="fa-trash" buttonColor="btn-danger" modalType="softDeleteAll" />
+        @endif
     @endcan
 </x-slot>
 
 <!-- Button Restore All -->
 <x-slot name="restoreAll">
     @can('restore-all:' . $permission)
-        <x-table-modal modalMethod="PUT" route="{{ route('admin.' . $permission . '.restoreAll') }}"
-            modalTitle="Restore All Data" modalText="Are you sure you want to restore all deleted data?"
-            buttonText="Restore All" buttonIcon="fa-undo" buttonColor="btn-info" modalType="restoreAll" />
+        @if (Route::has('admin.' . $permission . '.restoreAll'))
+            <x-table-modal modalMethod="PUT" route="{{ route('admin.' . $permission . '.restoreAll') }}"
+                modalTitle="Restore All Data" modalText="Are you sure you want to restore all deleted data?"
+                buttonText="Restore All" buttonIcon="fa-undo" buttonColor="btn-info" modalType="restoreAll" />
+        @endif
     @endcan
 </x-slot>
 
 <!-- Button Permanent Delete All -->
 <x-slot name="deleteAll">
     @can('delete-all:' . $permission)
-        <x-table-modal modalMethod="DELETE" route="{{ route('admin.' . $permission . '.destroyAll') }}"
-            modalTitle="Delete All Data" modalText="Are you sure you want permanently to delete all data?"
-            buttonText="Delete All" buttonIcon="fa-skull-crossbones" buttonColor="btn-danger" modalType="hardDeleteAll" />
+        @if (Route::has('admin.' . $permission . '.destroyAll'))
+            <x-table-modal modalMethod="DELETE" route="{{ route('admin.' . $permission . '.destroyAll') }}"
+                modalTitle="Delete All Data" modalText="Are you sure you want permanently to delete all data?"
+                buttonText="Delete All" buttonIcon="fa-skull-crossbones" buttonColor="btn-danger"
+                modalType="hardDeleteAll" />
+        @endif
     @endcan
 </x-slot>
 
