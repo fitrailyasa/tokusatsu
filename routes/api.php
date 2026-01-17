16de1b43f75dbfcf55330a2d4ac2ca6a890d33da
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2026-01-17 13:08:40 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -11,32 +11,31 @@ use App\Http\Controllers\Api\VideoApiController;
 
 Route::post('/login', [LoginApiController::class, 'loginApi'])->name('loginApi');
 
-// API
-Route::resource('/', DataApiController::class, ['only', ['index']]);
-
-// ERA API
-Route::resource('/eras', EraApiController::class, ['only', ['index', 'store', 'show', 'edit', 'update', 'destroy']]);
-Route::get('/era/all', [EraApiController::class, 'all']);
-
-// FRANCHISE API
-Route::resource('/franchises', FranchiseApiController::class, ['only', ['index', 'store', 'show', 'edit', 'update', 'destroy']]);
-Route::get('/franchise/all', [FranchiseApiController::class, 'all']);
-
-// CATEGORY API
-Route::resource('/categories', CategoryApiController::class, ['only', ['index', 'store', 'show', 'edit', 'update', 'destroy']]);
-Route::get('/category/all', [CategoryApiController::class, 'all']);
-Route::get('/category/era/{era}', [CategoryApiController::class, 'findByEra']);
-Route::get('/category/franchise/{franchise}', [CategoryApiController::class, 'findByFranchise']);
-
-// TAG API
-Route::resource('/tags', TagApiController::class, ['only', ['index', 'store', 'show', 'edit', 'update', 'destroy']]);
-Route::get('/tag/all', [TagApiController::class, 'all']);
-
-// DATA API
-Route::resource('/datas', DataApiController::class, ['only', ['index', 'store', 'show', 'edit', 'update', 'destroy']]);
-Route::get('/data/{franchise}/{category}', [DataApiController::class, 'findByFranchiseCategory']);
-
 Route::group(['middleware' => ['auth:sanctum']], function () {
+    // API
+    Route::resource('/', DataApiController::class, ['only', ['index']]);
+
+    // ERA API
+    Route::resource('/eras', EraApiController::class, ['only', ['index', 'store', 'show', 'edit', 'update', 'destroy']]);
+    Route::get('/era/all', [EraApiController::class, 'all']);
+
+    // FRANCHISE API
+    Route::resource('/franchises', FranchiseApiController::class, ['only', ['index', 'store', 'show', 'edit', 'update', 'destroy']]);
+    Route::get('/franchise/all', [FranchiseApiController::class, 'all']);
+
+    // CATEGORY API
+    Route::resource('/categories', CategoryApiController::class, ['only', ['index', 'store', 'show', 'edit', 'update', 'destroy']]);
+    Route::get('/category/all', [CategoryApiController::class, 'all']);
+    Route::get('/category/era/{era}', [CategoryApiController::class, 'findByEra']);
+    Route::get('/category/franchise/{franchise}', [CategoryApiController::class, 'findByFranchise']);
+
+    // TAG API
+    Route::resource('/tags', TagApiController::class, ['only', ['index', 'store', 'show', 'edit', 'update', 'destroy']]);
+    Route::get('/tag/all', [TagApiController::class, 'all']);
+
+    // DATA API
+    Route::resource('/datas', DataApiController::class, ['only', ['index', 'store', 'show', 'edit', 'update', 'destroy']]);
+    Route::get('/data/{franchise}/{category}', [DataApiController::class, 'findByFranchiseCategory']);
 
     // VIDEO API
     Route::resource('/videos', VideoApiController::class, ['only', ['index', 'store', 'show', 'edit', 'update', 'destroy']]);
