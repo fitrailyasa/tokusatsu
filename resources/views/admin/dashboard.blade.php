ba274f869069988b9073f8bf0ad58a870af129db
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2026-01-19 02:59:33 +0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,110 +9,125 @@
 
     <!-- Content -->
     <div class="row">
-        <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-                <div class="inner">
-                    <h3>{{ $datas }}</h3>
+        @can('view:data')
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3>{{ $datas }}</h3>
 
-                    <p>{{ __('Data') }}</p>
+                        <p>{{ __('Data') }}</p>
+                    </div>
+                    <a href="{{ route('admin.data.index') }}" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
                 </div>
-                <a href="{{ route('admin.data.index') }}" class="small-box-footer">More info <i
-                        class="fas fa-arrow-circle-right"></i></a>
             </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-danger">
-                <div class="inner">
-                    <h3>{{ $categories }}</h3>
+        @endcan
 
-                    <p>{{ __('Category') }}</p>
-                </div>
-                <a href="{{ route('admin.category.index') }}" class="small-box-footer">More info <i
-                        class="fas fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-                <div class="inner">
-                    <h3>{{ $eras }}</h3>
+        @can('view:category')
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h3>{{ $categories }}</h3>
 
-                    <p>{{ __('Era') }}</p>
+                        <p>{{ __('Category') }}</p>
+                    </div>
+                    <a href="{{ route('admin.category.index') }}" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
                 </div>
-                <a href="{{ route('admin.era.index') }}" class="small-box-footer">More info <i
-                        class="fas fa-arrow-circle-right"></i></a>
             </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-danger">
-                <div class="inner">
-                    <h3>{{ $franchises }}</h3>
+        @endcan
 
-                    <p>{{ __('Franchise') }}</p>
-                </div>
-                <a href="{{ route('admin.franchise.index') }}" class="small-box-footer">More info <i
-                        class="fas fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-danger">
-                <div class="inner">
-                    <h3>{{ $videos }}</h3>
+        @can('view:era')
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3>{{ $eras }}</h3>
 
-                    <p>{{ __('Video') }}</p>
+                        <p>{{ __('Era') }}</p>
+                    </div>
+                    <a href="{{ route('admin.era.index') }}" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
                 </div>
-                <a href="{{ route('admin.video.index') }}" class="small-box-footer">More info <i
-                        class="fas fa-arrow-circle-right"></i></a>
             </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-                <div class="inner">
-                    <h3>{{ $tags }}</h3>
+        @endcan
 
-                    <p>{{ __('Tag') }}</p>
-                </div>
-                <a href="{{ route('admin.tag.index') }}" class="small-box-footer">More info <i
-                        class="fas fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-danger">
-                <div class="inner">
-                    <h3>{{ $roles }}</h3>
+        @can('view:franchise')
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h3>{{ $franchises }}</h3>
 
-                    <p>{{ __('Role') }}</p>
+                        <p>{{ __('Franchise') }}</p>
+                    </div>
+                    <a href="{{ route('admin.franchise.index') }}" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
                 </div>
-                <a href="{{ route('admin.role.index') }}" class="small-box-footer">More info <i
-                        class="fas fa-arrow-circle-right"></i></a>
             </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-                <div class="inner">
-                    <h3>{{ $users }}</h3>
+        @endcan
 
-                    <p>{{ __('User') }}</p>
+        @can('view:video')
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h3>{{ $videos }}</h3>
+
+                        <p>{{ __('Video') }}</p>
+                    </div>
+                    <a href="{{ route('admin.video.index') }}" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
                 </div>
-                <a href="{{ route('admin.user.index') }}" class="small-box-footer">More info <i
-                        class="fas fa-arrow-circle-right"></i></a>
             </div>
-        </div>
-        <!-- ./col -->
+        @endcan
+
+        @can('view:tag')
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3>{{ $tags }}</h3>
+
+                        <p>{{ __('Tag') }}</p>
+                    </div>
+                    <a href="{{ route('admin.tag.index') }}" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        @endcan
+
+        @can('view:role')
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h3>{{ $roles }}</h3>
+
+                        <p>{{ __('Role') }}</p>
+                    </div>
+                    <a href="{{ route('admin.role.index') }}" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        @endcan
+
+        @can('view:user')
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3>{{ $users }}</h3>
+
+                        <p>{{ __('User') }}</p>
+                    </div>
+                    <a href="{{ route('admin.user.index') }}" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        @endcan
     </div>
 
 </x-admin-layout>
