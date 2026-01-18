3543c6d98ef7e2ad4056b021ce43b89455cd624f
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2026-01-19 00:27:13 +0700

    update

--- config/queue.php
+++ config/queue.php
@@ -86,7 +86,7 @@ return [
     */
 
     'batching' => [
-        'database' => env('DB_CONNECTION', 'sqlite'),
+        'database' => env('DB_AUTH_CONNECTION', 'sqlite'),
         'table' => 'job_batches',
     ],
 
@@ -105,7 +105,7 @@ return [
 
     'failed' => [
         'driver' => env('QUEUE_FAILED_DRIVER', 'database-uuids'),
-        'database' => env('DB_CONNECTION', 'sqlite'),
+        'database' => env('DB_AUTH_CONNECTION', 'sqlite'),
         'table' => 'failed_jobs',
     ],
 
